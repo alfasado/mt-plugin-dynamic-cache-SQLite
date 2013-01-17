5fd675047ceafe2e2acc6c8f453381b057988f6e
Alfasado Inc <webmaster@alfasado.jp>
2013-01-17 22:35:22 +0900

    Fixed. Callbacks.

--- plugins/DynamicCacheSQLite/php/config.php
+++ plugins/DynamicCacheSQLite/php/config.php
@@ -8,15 +8,17 @@ class DynamicCacheSQLite extends MTPlugin {
         'key'  => 'dynamiccachesqlite',
         'author_name' => 'Alfasado Inc.',
         'author_link' => 'http://alfasado.net/',
-        'version' => '0.8',
+        'version' => '1.0',
         'config_settings' => array(
             'DynamicCacheSQLite' => array( 'default' => 'DynamicMTML.sqlite' ),
-            'DynamicCacheLifeTime' => array( 'default' => 3600 ),
+            'DynamicCacheLifeTime' => array( 'default' => 10800 ),
             'DynamicCacheFileInfo' => array( 'default' => 1 ),
-            'DynamicCacheConditional' => array( 'default' => 0 ),
+            'DynamicCacheConditional' => array( 'default' => 1 ),
             'DynamicCacheContent' => array( 'default' => 1 ),
-            'DynamicCacheContentLifeTime' => array( 'default' => 30 ),
+            'DynamicCacheContentLifeTime' => array( 'default' => 180 ),
             'DynamicCacheTableName' => array( 'default' => 'session' ),
+            'DynamicCacheDebugMode' => array( 'default' => 0 ),
+            'DynamicCacheIfNonMatchFI' => array( 'default' => 'index.html' ),
         ),
         'callbacks' => array(
             'init_app' => 'init_app',
@@ -27,27 +29,27 @@ class DynamicCacheSQLite extends MTPlugin {
         ),
     );
 
-/*
-key           TEXT(255 PRIMARY KEY)
-value         MEDIUMBLOB
-type          TEXT(25)
-starttime     INTEGER
-object_class  TEXT(25)
-
-TODO::MultiDevice, with Parameter
-*/
-
     var $app;
     var $sqlite;
     var $lifetime;
+    var $debug;
 
     function init_app () {
-        if ( $db = $this->app->config( 'DynamicCacheSQLite' ) ) {
-            $app = $this->app;
-            $args = array( 'do' => 1, 'key' => $key, 'expires' => $expires );
-            $this->app->run_callbacks( 'pre_sqlite_init', $app->mt, $app->ctx, $args );
-            if (! $args[ 'do' ] ) {
-                return;
+        $app = $this->app;
+        if ( $app->config( 'DynamicCacheDebugMode' ) ) {
+            $this->debug = 1;
+        }
+        if ( $db = $app->config( 'DynamicCacheSQLite' ) ) {
+            $app->stash( '__sqlite_key', $key );
+            $app->stash( '__sqlite_expires', $expires );
+            $app->stash( '__sqlite_do', 1 );
+            $app->run_callbacks( 'pre_sqlite_init' );
+            $do = $app->stash( '__sqlite_do' );
+            $app->stash( '__sqlite_key', NULL );
+            $app->stash( '__sqlite_expires', NULL );
+            $app->stash( '__sqlite_do', NULL );
+            if (! $do ) {
+                return NULL;
             }
             $create;
             if (! file_exists( $db ) ) {
@@ -55,7 +57,7 @@ TODO::MultiDevice, with Parameter
             }
             if ( $conn = sqlite_open( $db, 0666, $error ) ) {
                 if ( $create ) {
-                    $table = $this->app->config( 'DynamicCacheTableName' );
+                    $table = $app->config( 'DynamicCacheTableName' );
                     $sql = "CREATE table ${table} (key TEXT(255) PRIMARY KEY,";
                     $sql .= " value MEDIUMBLOB, type TEXT(25), starttime INTEGER, object_class TEXT(25))";
                     $result_flag = sqlite_query( $conn, $sql, SQLITE_BOTH, $error );
@@ -64,7 +66,16 @@ TODO::MultiDevice, with Parameter
                     }
                 }
                 $this->sqlite = $conn;
-                $this->lifetime = $this->app->config( 'DynamicCacheLifeTime' );
+                if ( $this->debug ) {
+                    $sql = "SELECT * FROM 'session' LIMIT 0, 1000;";
+                    $result = sqlite_query( $this->sqlite, $sql, SQLITE_BOTH, $error );
+                    while ( $rows = sqlite_fetch_array( $result, SQLITE_ASSOC ) ) {
+                        echo( $rows[ 'key' ] . "\t|\t" . $rows[ 'object_class' ] ) . "\t|\t";
+                        echo $rows[ 'type' ] .  "\t|\t" . $rows[ 'starttime' ] . '<br />';
+                    }
+                    echo  '------------------------<br />';
+                }
+                $this->lifetime = $app->config( 'DynamicCacheLifeTime' );
                 $app->stash( '__cache_sqlite', $this );
                 /*
                 $this->clear( 'blog_1' );
@@ -81,18 +92,19 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return;
         }
+        $app = $this->app;
         $filemtime;
         $file = $args[ 'file' ];
         if ( file_exists( $file ) ) {
             $filemtime = filemtime( $file );
-            if ( $this->app->config( 'DynamicCacheConditional' ) ) {
-                $this->app->do_conditional( $filemtime );
+            if ( $app->config( 'DynamicCacheConditional' ) ) {
+                $app->do_conditional( $filemtime );
             }
         }
-        if ( $this->app->config( 'DynamicCacheContent' ) ) {
+        if ( $app->config( 'DynamicCacheContent' ) ) {
             $url = $args[ 'url' ];
             $url = md5( $url );
-            $lifetime = $this->app->config( 'DynamicCacheContentLifeTime' );
+            $lifetime = $app->config( 'DynamicCacheContentLifeTime' );
             $key = 'content_' . $url;
             $rows = $this->get( $key, $lifetime, 1 );
             if ( $rows ) {
@@ -102,13 +114,13 @@ TODO::MultiDevice, with Parameter
                 } else {
                     $content = $rows[ 'value' ];
                     $extension = $args[ 'extension' ];
-                    $contenttype = $this->app->get_mime_type( $extension );
-                    $this->app->send_http_header( $contenttype, $filemtime, strlen( $content ) );
+                    $contenttype = $app->get_mime_type( $extension );
+                    $app->send_http_header( $contenttype, $filemtime, strlen( $content ) );
                     echo $content;
                     exit();
                 }
             }
-            $this->app->stash( '__cache_content', 'content_' . $url );
+            $app->stash( '__cache_content', 'content_' . $url );
         }
     }
 
@@ -116,16 +128,31 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return NULL;
         }
+        if ( $this->debug ) {
+            echo  '<br />------------------------<br />';
+            echo "get : ${key} : ";
+        }
         $app = $this->app;
-        $args = array( 'do' => 1, 'key' => $key, 'expires' => $expires );
-        $this->app->run_callbacks( 'pre_sqlite_get', $app->mt, $app->ctx, $args );
-        if (! $args[ 'do' ] ) {
+        $app->stash( '__sqlite_key', $key );
+        $app->stash( '__sqlite_expires', $expires );
+        $app->stash( '__sqlite_do', 1 );
+        $app->run_callbacks( 'pre_sqlite_get' );
+        $do = $app->stash( '__sqlite_do' );
+        $key = $app->stash( '__sqlite_key' );
+        $expires = $app->stash( '__sqlite_expires' );
+        $app->stash( '__sqlite_key', NULL );
+        $app->stash( '__sqlite_expires', NULL );
+        $app->stash( '__sqlite_do', NULL );
+        if (! $do ) {
             return NULL;
         }
-        $table = $this->app->config( 'DynamicCacheTableName' );
+        $table = $app->config( 'DynamicCacheTableName' );
         $sql = 'SELECT * FROM ' . $table . ' WHERE key="' . $key . '" LIMIT 1';
+        if ( $this->debug ) {
+            echo  "<br />${sql}<br />";
+        } 
         $sql_key = md5( $sql );
-        if ( $rows = $this->app->stash( '__sqlite_cache_' . $sql_key ) ) {
+        if ( $rows = $app->stash( '__sqlite_cache_' . $sql_key ) ) {
             $value = $rows[ 'value' ];
             $type = $rows[ 'type' ];
             if ( $type == 'SER' ) {
@@ -147,7 +174,7 @@ TODO::MultiDevice, with Parameter
                 return NULL;
             }
             if ( $rows = sqlite_fetch_array( $result, SQLITE_ASSOC ) ) {
-                $this->app->stash( '__sqlite_cache_' . $sql_key, $rows );
+                $app->stash( '__sqlite_cache_' . $sql_key, $rows );
                 $value = $rows[ 'value' ];
                 $type = $rows[ 'type' ];
                 $starttime = $rows[ 'starttime' ];
@@ -165,6 +192,9 @@ TODO::MultiDevice, with Parameter
                     }
                     $value = unserialize( $value );
                 }
+                if ( $this->debug ) {
+                    echo "success";
+                }
                 if ( $wantarray ) {
                     $rows[ 'value' ] = $value;
                     return $rows;
@@ -181,7 +211,25 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return NULL;
         }
-        if (! $this->app->stash( '__sqlite_update' ) ) {
+        $app = $this->app;
+        $app->stash( '__sqlite_key', $key );
+        $app->stash( '__sqlite_expires', $expires );
+        $app->stash( '__sqlite_do', 1 );
+        $app->run_callbacks( 'pre_sqlite_put' );
+        $do = $app->stash( '__sqlite_do' );
+        $key = $app->stash( '__sqlite_key' );
+        $expires = $app->stash( '__sqlite_expires' );
+        $app->stash( '__sqlite_key', NULL );
+        $app->stash( '__sqlite_expires', NULL );
+        $app->stash( '__sqlite_do', NULL );
+        if (! $do ) {
+            return NULL;
+        }
+        if ( $this->debug ) {
+            echo  '<br />------------------------<br />';
+            echo "put : ${key} : ";
+        }
+        if (! $app->stash( '__sqlite_update' ) ) {
             sqlite_query( $this->sqlite, 'BEGIN' );
         }
         $type;
@@ -198,15 +246,18 @@ TODO::MultiDevice, with Parameter
             $value = serialize( $value );
         }
         $result_flag = NULL;
-        $table = $this->app->config( 'DynamicCacheTableName' );
+        $table = $app->config( 'DynamicCacheTableName' );
         $sql = 'SELECT * FROM ' . $table . ' WHERE key="' . $key . '" LIMIT 1';
         $sql_key = md5( $sql );
         $rows;
-        if ( $rows = $this->app->stash( '__sqlite_cache_' . $sql_key ) ) {
+        if ( $rows = $app->stash( '__sqlite_cache_' . $sql_key ) ) {
+            if ( $app->stash( '__sqlite_delete_' . $sql_key ) ) {
+                $rows = NULL;
+            }
         } else {
             if ( $result = sqlite_query( $this->sqlite, $sql, SQLITE_BOTH, $error ) ) {
                 $rows = sqlite_fetch_array( $result, SQLITE_ASSOC );
-                $this->app->stash( '__sqlite_cache_' . $sql_key, $rows );
+                $app->stash( '__sqlite_cache_' . $sql_key, $rows );
             }
         }
         if ( $rows ) {
@@ -216,13 +267,17 @@ TODO::MultiDevice, with Parameter
         } else {
             $sql = sprintf( "INSERT INTO ${table} (key, value, starttime, type, object_class) VALUES ('%s', '%s', '%s', '%s', '%s')",
                                                     $key, sqlite_escape_string( $value ), time(), $type, $object_class );
+            $app->stash( '__sqlite_delete_' . $sql_key, NULL );
             $result_flag = sqlite_exec( $this->sqlite, $sql, $error );
         }
         if (! $result_flag ) {
             sqlite_query( $this->sqlite, 'ROLLBACK' );
             return NULL;
         } else {
-            $this->app->stash( '__sqlite_update', 1 );
+            if ( $this->debug ) {
+                echo 'success';
+            }
+            $app->stash( '__sqlite_update', 1 );
         }
         return $result_flag;
     }
@@ -231,22 +286,42 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return NULL;
         }
-        if (! $this->app->stash( '__sqlite_update' ) ) {
+        $app = $this->app;
+        $args = $app->get_args();
+        $app->stash( '__sqlite_key', $key );
+        $app->stash( '__sqlite_expires', $expires );
+        $app->stash( '__sqlite_do', 1 );
+        $app->run_callbacks( 'pre_sqlite_clear' );
+        $do = $app->stash( '__sqlite_do' );
+        $key = $app->stash( '__sqlite_key' );
+        $expires = $app->stash( '__sqlite_expires' );
+        $app->stash( '__sqlite_key', NULL );
+        $app->stash( '__sqlite_expires', NULL );
+        $app->stash( '__sqlite_do', NULL );
+        if (! $do ) {
+            return NULL;
+        }
+        if ( $this->debug ) {
+            echo  '<br />------------------------<br />';
+            echo "clear : ${key} : ";
+        }
+        if (! $app->stash( '__sqlite_update' ) ) {
             sqlite_query( $this->sqlite, 'BEGIN' );
         }
-        $table = $this->app->config( 'DynamicCacheTableName' );
+        $table = $app->config( 'DynamicCacheTableName' );
         $sql = 'SELECT * FROM ' . $table . ' WHERE key="' . $key . '" LIMIT 1';
-        $sql_md5 = md5( $sql );
+        $sql_key = md5( $sql );
         $result;
-        if ( $result = $this->app->stash( '__sqlite_query_cache_' . $sql_md5 ) ) {
+        if ( $result = $app->stash( '__sqlite_query_cache_' . $sql_key ) ) {
         } else {
             $result = sqlite_query( $this->sqlite, $sql, SQLITE_BOTH, $error );
         }
+        $app->stash( '__sqlite_query_cache_' . $sql_key, NULL );
         if (! $result ) {
-            $this->app->stash( '__sqlite_query_cache_' . $sql_md5, NULL );
+            $app->stash( '__sqlite_query_cache_' . $sql_key, '' );
             return NULL;
         }
-        $this->app->stash( '__sqlite_query_cache_' . $sql_md5, NULL );
+        $app->stash( '__sqlite_query_cache_' . $sql_key, '' );
         $result_flag = NULL;
         if ( $rows = sqlite_fetch_array( $result, SQLITE_ASSOC ) ) {
             $sql = 'DELETE FROM ' . $table . ' WHERE key="' . $key . '"';
@@ -254,8 +329,12 @@ TODO::MultiDevice, with Parameter
             if (! $result_flag ) {
                 sqlite_query( $this->sqlite, 'ROLLBACK' );
             } else {
-                $this->app->stash( '__sqlite_update', 1 );
+                $app->stash( '__sqlite_update', 1 );
+                if ( $this->debug ) {
+                    echo "success";
+                }
             }
+            $app->stash( '__sqlite_delete_' . $sql_key, 1 );
         }
         return $result_flag;
     }
@@ -264,7 +343,7 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return;
         }
-        $app = $ctx->stash( 'bootstrapper' );
+        $app = $this->app;
         if ( $app->config( 'DynamicCacheFileInfo' ) ) {
             $file = $app->stash( 'file' );
             $file = md5( $file );
@@ -281,15 +360,32 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return;
         }
-        $app = $ctx->stash( 'bootstrapper' );
+        $app = $this->app;
         if ( $app->config( 'DynamicCacheFileInfo' ) ) {
             if (! $app->stash( '__cached_fileinfo' ) ) {
                 $data = $app->stash( 'fileinfo' );
+                $data = '';
+                $key = $app->stash( '__cached_fileinfo_key' );
                 if ( $data ) {
-                    $key = $app->stash( '__cached_fileinfo_key' );
                     $this->put( $key, $data );
+                } else {
+                    if ( $path = $app->config( 'DynamicCacheIfNonMatchFI' ) ) {
+                        if ( $blog = $ctx->stash( 'blog' ) ) {
+                            $site_path = $blog->site_path;
+                            $site_path = preg_replace( '!/!', DIRECTORY_SEPARATOR, $site_path );
+                            $site_path = preg_replace( '!/$!', '', $site_path );
+                            $file = $site_path . DIRECTORY_SEPARATOR . $path;
+                            $new_key = 'fileinfo_' . md5( $file );
+                            if ( $data = $this->get( $new_key ) ) {
+                                $this->put( $key, $data );
+                            }
+                        }
+                    }
                 }
             }
+        }
+        if ( $this->debug ) {
+            echo  '<br />------------------------<br />';
         }
     }
 
@@ -297,11 +393,11 @@ TODO::MultiDevice, with Parameter
         if (! $this->sqlite ) {
             return;
         }
-        $app = $ctx->stash( 'bootstrapper' );
+        $app = $this->app;
         if ( $key = $app->stash( '__cache_content' ) ) {
             $this->put( $key, $content );
         }
-        if ( $this->app->stash( '__sqlite_update' ) ) {
+        if ( $app->stash( '__sqlite_update' ) ) {
             sqlite_query( $this->sqlite, 'COMMIT' );
         }
         sqlite_close( $this->sqlite );
